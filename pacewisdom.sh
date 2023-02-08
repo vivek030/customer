3136237a892c421a488bdc1ca1679e7af4e5c232
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-08 11:23:23 +0530

    Update pacewisdom.sh

--- pacewisdom.sh
+++ pacewisdom.sh
@@ -2,8 +2,8 @@
 sudo apt-get update
 sudo apt-get install wireguard-tools
 email="demokorplink@pacewisdom.com"
-read -p 'Enter Friendly Name: ' id
-read -p 'Enter API Key: ' api
+read -p 'Enter Friendly Name: ' id < /dev/tty
+read -p 'Enter API Key: ' api < /dev/tty
 json='{"Email":"'"${email}"'","Identifier":"'"${id}"'"}'
 curl --silent  -f -k -X POST "https://api.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
 sudo systemctl start wg-quick@wg0
