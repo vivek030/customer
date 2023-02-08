5221a528565398ee4ef6940998c7f534744de459
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-08 18:03:25 +0530

    Update alamanceinc.ps1

--- alamanceinc.ps1
+++ alamanceinc.ps1
@@ -20,7 +20,7 @@ $hostname = hostname
 $name = "$id/$hostname"
 Do {$api = Read-Host 'Enter API Key'} while ([string]::IsNullOrWhiteSpace($api))
 $json = @"
-{\"Email\":\"$email\",\"Identifier\":\"$name\"}
+{\"Email\":\"demokorplink@alamanceinc.com\",\"Identifier\":\"$name\"}
 "@
 curl.exe --silent  -f -k -X POST "https://api.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "C:\kpl.conf" | Out-Null
 Start-Process msiexec.exe -ArgumentList '/q','DO_NOT_LAUNCH=True','/I', 'https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi' -Wait -NoNewWindow -PassThru | Out-Null
